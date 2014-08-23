aca5392df627ef9c491d50388b620d377ed2c849
Ilya Murzinov <murz42@gmail.com>
2014-08-23 17:02:46 +0400

    [feature] add option to select language to settings

---
+++ wail-app/src/main/java/com/artemzin/android/wail/ui/activity/settings/SettingsSelectLanguageActivity.java
@@ -0,0 +1,13 @@
+package com.artemzin.android.wail.ui.activity.settings;
+
+import android.os.Bundle;
+
+import com.artemzin.android.wail.R;
+
+public class SettingsSelectLanguageActivity extends BaseSettingsActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_select_language);
+    }
+}
---
+++ wail-app/src/main/java/com/artemzin/android/wail/ui/fragment/settings/SettingsSelectLanguageFragment.java
@@ -0,0 +1,66 @@
+package com.artemzin.android.wail.ui.fragment.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.artemzin.android.wail.R;
+import com.artemzin.android.wail.ui.fragment.BaseFragment;
+import com.artemzin.android.wail.util.LocaleUtil;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
+
+import java.util.Locale;
+
+public class SettingsSelectLanguageFragment extends BaseFragment implements ListView.OnItemClickListener {
+
+    private final String GA_EVENT_SETTINGS_SELECT_LANGUAGE = "SettingsSelectLanguage";
+
+    private String[] languages;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        languages = getResources().getStringArray(R.array.settings_select_language_languages);
+        languages[0] = String.format(languages[0] + " (%s)", Locale.getDefault().getDisplayLanguage());
+        ListView languagesList = (ListView) view.findViewById(R.id.settings_select_language_list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                languages
+        );
+        languagesList.setAdapter(adapter);
+        languagesList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings_select_language, container, false);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i) {
+            case 0:
+                LocaleUtil.updateLanguage(getActivity(), Locale.getDefault().getDisplayLanguage());
+                break;
+            default:
+                LocaleUtil.updateLanguage(getActivity(), languages[i]);
+                break;
+        }
+
+        EasyTracker.getInstance(getActivity()).send(MapBuilder.createEvent(
+                GA_EVENT_SETTINGS_SELECT_LANGUAGE,
+                "languageChangedTo",
+                i == 0 ? "default" : languages[i],
+                0L
+        ).build());
+
+        getActivity().finish();
+    }
+}
