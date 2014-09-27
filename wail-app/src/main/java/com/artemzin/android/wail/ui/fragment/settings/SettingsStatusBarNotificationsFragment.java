df98f2ce3bf611fbcb98bf9500adbc2a5aa60a87
Ilya Murzinov <murz42@gmail.com>
2014-09-27 20:35:58 +0400

    [rft] settings fragments refactoring

--- wail-app/src/main/java/com/artemzin/android/wail/ui/fragment/settings/SettingsStatusBarNotificationsFragment.java
+++ wail-app/src/main/java/com/artemzin/android/wail/ui/fragment/settings/SettingsStatusBarNotificationsFragment.java
@@ -1,11 +1,9 @@
 package com.artemzin.android.wail.ui.fragment.settings;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.artemzin.android.wail.R;
@@ -16,6 +14,10 @@ import com.artemzin.android.wail.ui.fragment.BaseFragment;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.MapBuilder;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnCheckedChanged;
+
 /**
  * @author Ilya Murzinov [murz42@gmail.com]
  */
@@ -23,41 +25,41 @@ public class SettingsStatusBarNotificationsFragment extends BaseFragment {
 
     private final String GA_EVENT_SETTINGS_STATUS_BAR_NOTIFICATIONS = "SettingsStatusBarNotifications";
 
+    @InjectView(R.id.settings_status_bar_notifications_track_now_scrobbling_switch)
+    public Switch trackNowScrobblingStatusBarNotificationsSwitch;
+
+    @OnCheckedChanged(R.id.settings_status_bar_notifications_track_now_scrobbling_switch)
+    public void onStatusBarNotificationSwitchChanged(boolean isChecked) {
+        WAILSettings.setStatusBarNotificationTrackScrobblingEnabled(getActivity(), isChecked);
+
+        if (isChecked) {
+            Track track = WAILSettings.getNowScrobblingTrack(getActivity());
+            if (track != null) {
+                StatusBarNotificationsManager.getInstance(getActivity())
+                        .showTrackScrobblingStatusBarNotification(track);
+            }
+        } else {
+            StatusBarNotificationsManager.getInstance(getActivity()).cancelAllNotifications();
+        }
+
+        EasyTracker.getInstance(getActivity()).send(MapBuilder.createEvent(GA_EVENT_SETTINGS_STATUS_BAR_NOTIFICATIONS,
+                "nowPlayingStatusBarNotifications",
+                isChecked ? "enabled" : "disabled",
+                isChecked ? 1L : 0L).build());
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings_status_bar_notifications, container, false);
+        View view = inflater
+                .inflate(R.layout.fragment_settings_status_bar_notifications, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        final Activity activity = getActivity();
-
-        Switch trackNowScrobblingStatusBarNotificationsSwitch =
-                (Switch) view.findViewById(R.id.settings_status_bar_notifications_track_now_scrobbling_switch);
-        trackNowScrobblingStatusBarNotificationsSwitch.setChecked(WAILSettings.isStatusBarNotificationTrackScrobblingEnabled(activity));
-        trackNowScrobblingStatusBarNotificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                WAILSettings.setStatusBarNotificationTrackScrobblingEnabled(activity, isChecked);
-
-                if (isChecked) {
-                    Track track = WAILSettings.getNowScrobblingTrack(getActivity());
-                    if (track != null)
-                    {
-                        StatusBarNotificationsManager.getInstance(getActivity())
-                                .showTrackScrobblingStatusBarNotification(track);
-                    }
-                } else {
-                    StatusBarNotificationsManager.getInstance(getActivity()).cancelAllNotifications();
-                }
-
-                EasyTracker.getInstance(activity).send(MapBuilder.createEvent(GA_EVENT_SETTINGS_STATUS_BAR_NOTIFICATIONS,
-                        "nowPlayingStatusBarNotifications",
-                        isChecked ? "enabled" : "disabled",
-                        isChecked? 1L : 0L).build());
-            }
-        });
+        trackNowScrobblingStatusBarNotificationsSwitch
+                .setChecked(WAILSettings.isStatusBarNotificationTrackScrobblingEnabled(getActivity()));
     }
 }
